ae3565f882fab2909acaf013c782974d9dc88baa
chl55 <clementchlau@gmail.com>
2026-04-08 23:47:15 -0700

    Fully isolate client underlay

--- MiniProject/client.py
+++ MiniProject/client.py
@@ -1,47 +1,39 @@
-import socket
 import sys
 from utils.network_config import ClientNetworkConfig
 from utils.cpp import decode_cpp, encode_cpp, CPPStatus
-from utils.client_transport_underlay import send_cpp_packet
-from utils.globals import Resource, HOST, STREAM_RESPONSE_COUNT
-
-BUFFER_SIZE = 4096
-TIMEOUT = 5
+from utils.client_transport_underlay import MessageTimeoutError, cleanup_underlay, client_receive_cpp_packet, send_cpp_packet, setup_underlay
+from utils.globals import Resource, STREAM_RESPONSE_COUNT
 
 def main() -> None:
-    if len(sys.argv) != 4:
-        print("Usage: python client.py <client_id> <resource> <port>")
-        print("Example: python client.py 1 page 8080")
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <client_id> <resource>")
+        print("Example: python client.py 1 page")
         return
 
     client_id = int(sys.argv[1])
     resource = Resource[sys.argv[2].upper()]
-    port = int(sys.argv[3])
 
     config = ClientNetworkConfig()
     if not config.is_authorized(client_id):
         print("Unauthorized client. Use another client ID.")
         return
     
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.bind((HOST, port))
+    setup_underlay(client_id)
     cpp_message = encode_cpp(source_id=client_id, resource=resource)
-    send_cpp_packet(dest_id=config.proxy_id, message=cpp_message, socket=client_socket)
+    send_cpp_packet(dest_id=config.proxy_id, message=cpp_message)
     
     # For STREAM resource, we expect 10 (defined in globals.py) responses spaced 1s apart. Only 1 response for the rest.
     response_count = STREAM_RESPONSE_COUNT if resource == Resource.STREAM else 1
 
     for _ in range(response_count):
-        client_socket.settimeout(TIMEOUT)
-        
         try:
-            response = decode_cpp(client_socket.recvfrom(BUFFER_SIZE)[0])
+            response = decode_cpp(client_receive_cpp_packet())
             if (response['source_id'] != config.proxy_id):
                 print("Received response not from proxy (id = {})! Dubious Sender ID:".format(config.proxy_id), response['source_id'])
                 return
             match(response['status']):
                 case CPPStatus.SUCCESS:
-                    print("Request successful:", response['payload'])
+                    print("Request successful (Status code {}):".format(CPPStatus.SUCCESS.value), response['payload'])
                 case CPPStatus.SERVER_BUSY:
                     print("Proxy indicates backend server is busy. Status code {}.".format(CPPStatus.SERVER_BUSY.value))
                     return
@@ -54,12 +46,11 @@ def main() -> None:
                 case CPPStatus.INVALID_REQUEST:
                     print("Invalid request sent to proxy. Status code {}.".format(CPPStatus.INVALID_REQUEST.value))
                     return
-        except socket.timeout:
-            print("Timed out: No response received within timeout period of {} seconds.".format(TIMEOUT))
+        except MessageTimeoutError as e:
+            print(e)
             return
 
-    client_socket.close()
-
-
+    cleanup_underlay()
+    
 if __name__ == "__main__":
     main()
--- MiniProject/utils/globals.py
+++ MiniProject/utils/globals.py
@@ -4,6 +4,7 @@ import base64
 HOST = "127.0.0.1"
 BUFFER_SIZE = 4096
 STREAM_RESPONSE_COUNT = 10
+TIMEOUT = 5  # secondsz
 STREAM_RESPONSE_INTERVAL = 1  # seconds
 LOGGING_DIR = "logs"
 
